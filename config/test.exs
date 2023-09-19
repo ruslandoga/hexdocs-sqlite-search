3d223643c25bbf343b97539b3d05ff687760e37f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-09-19 17:50:51 +0700

    cleanup

--- config/test.exs
+++ config/test.exs
@@ -1,16 +1,11 @@
 import Config
 
 # Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :wat, Wat.Repo,
-  database: Path.expand("../wat_test.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../wat2.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool: Ecto.Adapters.SQL.Sandbox
+  show_sensitive_data_on_connection_error: true
 
 # Print only warnings and errors during test
 config :logger, level: :warning
--- mix.exs
+++ mix.exs
@@ -10,7 +10,6 @@ defmodule Wat.MixProject do
       compilers: [:elixir_make | Mix.compilers()],
       start_permanent: Mix.env() == :prod,
       releases: releases(),
-      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -35,49 +34,11 @@ defmodule Wat.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      # {:phoenix, "~> 1.7.7"},
-      # {:phoenix_ecto, "~> 4.4"},
-      # {:ecto_sql, "~> 3.10"},
       {:ecto_sqlite3, ">= 0.0.0"},
-      # {:phoenix_html, "~> 3.3"},
-      # {:phoenix_live_reload, "~> 1.2", only: :dev},
-      # {:phoenix_live_view, "~> 0.19.0"},
-      # {:floki, ">= 0.30.0", only: :test},
-      # {:finch, "~> 0.13"},
-      # {:telemetry_metrics, "~> 0.6"},
-      # {:telemetry_poller, "~> 1.0"},
-      # {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      # {:earmark, "~> 1.4"},
-      # {:sqlite_vss, "~> 0.1.2"}
-      # {:hnswlib, github: "elixir-nx/hnswlib"},
-      # {:libgraph, "~> 0.16.0"},
-      # {:ex_sqlean, "~> 0.8.8"},
       {:corsica, "~> 2.1"},
       {:elixir_make, "~> 0.7.6"}
-    ]
-  end
-
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
-  defp aliases do
-    [
-      setup: ["deps.get", "ecto.setup", "assets.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate --log-migrations-sql"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["cmd npm ci --prefix assets"],
-      "assets.deploy": [
-        "cmd npm ci --prefix assets",
-        "cmd npm run deploy:css --prefix assets",
-        "cmd npm run deploy:js --prefix assets",
-        "phx.digest"
-      ]
     ]
   end
 
